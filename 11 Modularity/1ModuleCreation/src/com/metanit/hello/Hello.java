d0c444c96f12f544cf595ac90220d0d5a7f6f647
Dmitry Kulazhenko <58825576+DiKy001@users.noreply.github.com>
2021-07-07 11:13:11 +0300

    Add files via upload

---
+++ 11 Modularity/1ModuleCreation/src/com/metanit/hello/Hello.java
@@ -0,0 +1,7 @@
+package com.metanit.hello;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Demo Module!");
+    }
+}
---
+++ 11 Modularity/2DependentModules/src/com/metanit/hello/Hello.java
@@ -0,0 +1,11 @@
+package com.metanit.hello;
+
+import javax.swing.JOptionPane;
+
+public class Hello{
+
+    public static void main(String[] args){
+        //System.out.println("Hello Demo Module!");
+        JOptionPane.showMessageDialog(null, "Hello Demo Module!");
+    }
+}
